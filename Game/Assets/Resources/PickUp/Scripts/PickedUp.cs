572659f50a599cafa3a7be18dc40989dc174c21d
jeremybond <jeremybond2@hotmail.com>
2014-10-21 21:17:51 +0200

    simple pickup
    just created a simple pickup

---
+++ Game/Assets/Resources/PickUp/Scripts/PickedUp.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickedUp : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if(col.collider.tag == "Player")
+			Destroy(this.gameObject);
+	}
+}
