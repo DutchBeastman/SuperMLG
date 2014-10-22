afbc3cd2d9f9f287aec2fc5b152e050e5490e86b
kerim birlik <ninja_sky1@hotmail.com>
2014-10-22 23:36:51 +0200

    Level1.1 commit
    respawn added and things moved

---
+++ Game/Assets/Resources/Scripts/PlayerRespawn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRespawn : MonoBehaviour {
+
+	public Transform respawnPoint;
+	public GameObject player;
+	public GameObject checkpointMover;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.collider.tag == "KillingObject"){
+			player.transform.position = respawnPoint.position;
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.collider.tag == "KillingObject"){
+			player.transform.position = respawnPoint.position;
+		}
+		if(col.collider.gameObject == checkpointMover){
+			respawnPoint.transform.position = checkpointMover.transform.position;
+		}
+	}
+
+
+
+}
